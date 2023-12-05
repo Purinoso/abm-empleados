da0ecce53b735cc97154cc55bb2196f95e44a148
Purinoso <chomper.xy.123@gmail.com>
2023-12-05 08:26:57 -0300

    Creación de componente "lista-empleados"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ListaEmpleadosComponent } from '@/components/lista-empleados/lista-empleados.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'empleado/list', pathMatch: 'full' },
+    { path: 'empleado/list', component: ListaEmpleadosComponent },
+    { path: '**', redirectTo: 'empleado/list' }
+];
---
+++ src/app/components/lista-empleados/lista-empleados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-empleados',
+  standalone: true,
+  imports: [],
+  templateUrl: './lista-empleados.component.html',
+  styleUrl: './lista-empleados.component.css'
+})
+export class ListaEmpleadosComponent {
+
+}
